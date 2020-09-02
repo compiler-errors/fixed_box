cb794becac4a4a8d8e5a56c72f623e88e7eb43d3
Michael Goulet <michael@errs.io>
2020-09-01 19:20:59 -0700

    Use MaybeUninit, avoid Sized requirement on resize

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use core::{
     any::{type_name, Any},
     borrow::{Borrow, BorrowMut},
     marker::Unsize,
-    mem::{align_of, forget, size_of, ManuallyDrop},
+    mem::{align_of, forget, size_of, size_of_val, MaybeUninit},
     ops::{CoerceUnsized, Deref, DerefMut},
     ptr,
 };
@@ -46,24 +46,21 @@ impl<T, const N: usize> FixedBox<T, N> {
             N
         );
 
-        // TODO(compiler-errors): Is it safe to initialize the FixedBox like
-        // this, because the only unsafe action we can perform on this object
-        // would be to `Deref<Target=T>` it or drop it...
-        let mut newbox: ManuallyDrop<FixedBox<T, N>> = ManuallyDrop::new(FixedBox {
-            fake_ptr: align_of::<T>() as *const T,
-            _align: [],
-            contents: [0; N],
-        });
+        let mut new: MaybeUninit<FixedBox<T, N>> = MaybeUninit::zeroed();
 
         // Write our contents `t` into the box. This also ensures `t` won't
         // get dropped, since this function consumes it.
         unsafe {
-            ptr::write(newbox.contents.as_mut_ptr() as *mut T, t);
-        }
+            let fake_ptr = &mut (*new.as_mut_ptr()).fake_ptr as *mut *const T;
+            ptr::write(fake_ptr, align_of::<T>() as *const T);
 
-        // Finally, we can unwrap the box. From now on, it can be used without
-        // worries.
-        ManuallyDrop::into_inner(newbox)
+            let contents_ptr = (*new.as_mut_ptr()).contents.as_mut_ptr() as *mut T;
+            ptr::write(contents_ptr, t);
+
+            // Finally, we can unwrap the box. From now on, it can be used without
+            // worries.
+            new.assume_init()
+        }
     }
 
     /// Try to box the given `t`, or return it back in an `Err` if the box is
@@ -79,59 +76,6 @@ impl<T, const N: usize> FixedBox<T, N> {
             Err(t)
         }
     }
-
-    #[must_use]
-    /// Resize a box. Can be either smaller or larger, but must be large enough
-    /// to hold the contents of the box.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the new box size `M` is too small to hold the sized type `T`.
-    pub fn resize<const M: usize>(self) -> FixedBox<T, M> {
-        assert!(
-            size_of::<T>() <= M,
-            "The size of contents `{}` ({} bytes) is too small for resized \
-            FixedBox ({} bytes).",
-            type_name::<T>(),
-            size_of::<T>(),
-            N
-        );
-
-        let mut newbox = ManuallyDrop::new(FixedBox {
-            fake_ptr: self.fake_ptr,
-            _align: [],
-            contents: [0; M],
-        });
-
-        if M < N {
-            // This is a resize down.
-            newbox.contents.copy_from_slice(&self.contents[0..M]);
-        } else {
-            // This is a resize up.
-            newbox.contents[0..N].copy_from_slice(&self.contents);
-        }
-
-        // We should forget `self` before grabbing the contents of `newbox`.
-        // That ensures that if we panic somewhere in this method, then we won't
-        // ever drop two copies of the boxed object.
-        forget(self);
-        ManuallyDrop::into_inner(newbox)
-    }
-
-    /// Try to resize a box, or return it back in an `Err` if the new size is
-    /// too small.
-    //
-    /// # Errors
-    ///
-    /// Will return self as `Err` if contents are too small to fit in the
-    /// new `FixedBox`.
-    pub fn try_resize<const M: usize>(self) -> Result<FixedBox<T, M>, Self> {
-        if size_of::<T>() <= M {
-            Ok(self.resize())
-        } else {
-            Err(self)
-        }
-    }
 }
 
 impl<T: ?Sized, const N: usize> FixedBox<T, N> {
@@ -143,22 +87,50 @@ impl<T: ?Sized, const N: usize> FixedBox<T, N> {
     ///
     /// Panics if the new box size `M` is smaller than the box size `N`.
     #[must_use]
-    pub fn resize_up<const M: usize>(self) -> FixedBox<T, M> {
+    pub fn resize<const M: usize>(self) -> FixedBox<T, M> {
+        let real_size = size_of_val::<T>(&*self);
         assert!(
-            M >= N,
-            "Must use `FixedBox::resize()` to make a FixedBox smaller."
+            real_size <= M,
+            "The size of contents `{}` ({} bytes) is too small for resized \
+            FixedBox ({} bytes).",
+            type_name::<T>(),
+            real_size,
+            N
         );
 
-        let mut newbox = ManuallyDrop::new(FixedBox {
-            _align: [],
-            fake_ptr: self.fake_ptr,
-            contents: [0; M],
-        });
+        let mut new: MaybeUninit<FixedBox<T, M>> = MaybeUninit::zeroed();
 
-        newbox.contents[0..N].copy_from_slice(&self.contents);
+        unsafe {
+            // Write the fake ptr into the struct
+            let fake_ptr = &mut (*new.as_mut_ptr()).fake_ptr as *mut *const T;
+            ptr::write(fake_ptr, self.fake_ptr);
 
-        forget(self);
-        ManuallyDrop::into_inner(newbox)
+            // Copy the contents of the fake box. Nonoverlapping because we
+            // know for certain that this `new` is distinct, lol.
+            let contents_ptr = (*new.as_mut_ptr()).contents.as_mut_ptr();
+            ptr::copy_nonoverlapping(self.contents.as_ptr(), contents_ptr, real_size);
+
+            // We should forget `self` before grabbing the contents of `new`.
+            // That ensures that if we panic somewhere in this method, then we won't
+            // ever drop two copies of the boxed object.
+            forget(self);
+            new.assume_init()
+        }
+    }
+
+    /// Try to resize a box, or return it back in an `Err` if the new size is
+    /// too small.
+    //
+    /// # Errors
+    ///
+    /// Will return self as `Err` if contents are too small to fit in the
+    /// new `FixedBox`.
+    pub fn try_resize<const M: usize>(self) -> Result<FixedBox<T, M>, Self> {
+        if size_of_val::<T>(&*self) <= M {
+            Ok(self.resize())
+        } else {
+            Err(self)
+        }
     }
 }
 
@@ -187,30 +159,15 @@ impl<T: ?Sized, const N: usize> Deref for FixedBox<T, N> {
         // A pointer is either thin or fat.
         // If it's thin, then it'll occupy one usize.
         // If it's fat, then it'll occupy two.
-        assert!(
-            size_of::<*const T>() == size_of::<usize>()
-                || size_of::<*const T>() == 2 * size_of::<usize>()
-        );
+        assert!(size_of::<*const T>() % size_of::<usize>() == 0);
 
-        let mut pointer: [usize; 2] = [0, 0];
+        let mut ptr: MaybeUninit<*const T> = MaybeUninit::new(self.fake_ptr);
 
         unsafe {
-            // Copy either 1 or 2 "usizes" of pointer, depending on if T is fat.
-            // The purpose of this is that pointer[1] will contain the vtable
-            // or size, if needed. pointer[0] actually just contains the
-            // alignment, which we don't really care about...
-            ptr::copy(
-                &self.fake_ptr as *const *const T as *const usize,
-                pointer.as_mut_ptr(),
-                size_of::<*const T>() / size_of::<usize>(),
-            );
+            ptr::write(ptr.as_mut_ptr() as *mut *const u8, self.contents.as_ptr());
 
-            // Patch up the "pointer" to point to the actual object.
-            // This is hacky and makes me sad, but it works.
-            pointer[0] = self.contents.as_ptr() as usize;
-
-            // Reinterpret the `pointer` usize array as a `*const T`.
-            &*ptr::read(pointer.as_ptr() as *const *const T)
+            // Reinterpret the `ptr` as a `*const T`.
+            &*ptr.assume_init()
         }
     }
 }
@@ -218,15 +175,19 @@ impl<T: ?Sized, const N: usize> Deref for FixedBox<T, N> {
 impl<T: ?Sized, const N: usize> DerefMut for FixedBox<T, N> {
     // Essentially a code duplicate of the Deref impl.
     fn deref_mut(&mut self) -> &mut T {
-        let mut pointer: [usize; 2] = [0, 0];
+        // TODO(compiler-errors): Is this an assumption we can only make on x86?
+        // A pointer is either thin or fat.
+        // If it's thin, then it'll occupy one usize.
+        // If it's fat, then it'll occupy two.
+        assert!(size_of::<*mut T>() % size_of::<usize>() == 0);
+
+        let mut ptr: MaybeUninit<*mut T> = MaybeUninit::new(self.fake_ptr as *mut T);
+
         unsafe {
-            ptr::copy(
-                &self.fake_ptr as *const *const T as *const usize,
-                pointer.as_mut_ptr(),
-                size_of::<*const T>() / size_of::<usize>(),
-            );
-            pointer[0] = self.contents.as_ptr() as usize;
-            &mut *ptr::read(pointer.as_ptr() as *const *mut T)
+            ptr::write(ptr.as_mut_ptr() as *mut *mut u8, self.contents.as_mut_ptr());
+
+            // Reinterpret the `ptr` as a `*const T`.
+            &mut *ptr.assume_init()
         }
     }
 }
@@ -239,25 +200,33 @@ impl<const N: usize> FixedBox<dyn Any + 'static, N> {
     /// Will return self back as `Err` if the boxed value is not type `T`.
     pub fn downcast<T: 'static>(self) -> Result<FixedBox<T, N>, Self> {
         if (*self).is::<T>() {
-            // Basically just make a new `FixedBox<T>`...
-            let mut newbox = ManuallyDrop::new(FixedBox {
-                _align: [],
-                fake_ptr: align_of::<T>() as *const T,
-                contents: [0; N],
-            });
-
-            // Copy full "contents" slice.
-            newbox.contents.copy_from_slice(&self.contents);
-
-            // We need to forget `self` so we don't accidentally call the
-            // drop method of the contained contents.
-            forget(self);
-
-            // Now let's unwrap and return the newbox.
-            Ok(ManuallyDrop::into_inner(newbox))
+            Ok(unsafe { self.downcast_unsafe::<T>() })
         } else {
             Err(self)
         }
+    }
+
+    /// Downcast a `FixedBox<dyn Any>` into a concrete sybtype.
+    ///
+    /// # Safety
+    ///
+    /// The target type `T` _must_ be the actual type that is erased by the
+    /// `dyn Any`, otherwise this is as unsafe as a transmute.
+    #[must_use]
+    pub unsafe fn downcast_unsafe<T: 'static>(self) -> FixedBox<T, N> {
+        let mut new: MaybeUninit<FixedBox<T, N>> = MaybeUninit::zeroed();
+
+        let fake_ptr = &mut (*new.as_mut_ptr()).fake_ptr as *mut *const T;
+        ptr::write(fake_ptr, align_of::<T>() as *const T);
+
+        let contents_ptr = (*new.as_mut_ptr()).contents.as_mut_ptr();
+        ptr::copy_nonoverlapping(self.contents.as_ptr(), contents_ptr, size_of::<T>());
+
+        // We should forget `self` before grabbing the contents of `new`.
+        // That ensures that if we panic somewhere in this method, then we won't
+        // ever drop two copies of the boxed object.
+        forget(self);
+        new.assume_init()
     }
 }
 
